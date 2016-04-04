18e67ebc095fa10fc132d2a6854faa2c59ff667d
Jamie Hazelden <jhazelden@flextech-hs.org>
2016-04-04 14:08:48 -0400

    testing ssh

--- ConfigReader/Tokenizer/tokenizer.cpp
+++ ConfigReader/Tokenizer/tokenizer.cpp
@@ -1,10 +1,62 @@
+#include "tokenizer.h"
+#include <iostream>
 #ifndef cassert
 #include <cassert>
 #endif 
 
-void Symbol::setParams (std::vector<double> const &newParams) {
-   assert(mParams.size() == newParams.size());
-   for (int i = 0; i < mParams.size(); i++) {
-      mParams[i] = newParams[i];
+GLSstring Tokenizer::readAxiom ( std::ifstream &configFile ) {
+   std::string line;
+   GLSstring axiom;
+   while ( getline (configFile, line) ) {
+      if ( line.find ("axiom") != std::string::npos ) {
+         if ( getline (configFile, line) ) {
+            std::string::iterator letter {line.begin()};
+
+            if (*letter == ' ') { // whitespace
+               while (*(++letter) == ' ') {} 
+            }
+
+            while (*letter != ';') {
+               std::string identifier {*letter};
+               while (*(letter++) != ')') { // Name and params
+                  identifier.push_back(*letter);
+               } 
+
+               std::string * identPtr {new std::string(identifier)};
+               IdentifierToken * identToken {new IdentifierToken (identPtr)};
+               axiom.push_back( identToken );
+
+               if (*letter == ' ') { // whitespace
+                  while (*(++letter) == ' ') {} 
+               }
+            }
+         }
+         return axiom;
+      }
    }
-} 
+}
+
+std::vector<GLSstring> Tokenizer::readProductions ( std::ifstream &configFile ) {
+   return {};
+}
+
+Tokenizer::~Tokenizer () {
+   for ( auto const &token: mAxiom ) {
+      delete token;
+   }
+   for ( auto const &production: mProductions ) {
+      for ( auto const &token: production) {
+         delete token;
+      }
+   }
+}
+
+int main () {
+   std::ifstream fl;
+   fl.open("./config.GLS");
+   Tokenizer tk (fl);
+   GLSstring axiom {tk.getAxiom()};
+   for (auto const &name: axiom) {
+      std::cout<<*(static_cast<IdentifierToken*>(name)->getData())<<std::endl;
+   }
+}
--- ConfigReader/Tokenizer/tokenizer.h
+++ ConfigReader/Tokenizer/tokenizer.h
@@ -1,40 +1,98 @@
 #include <string>
 #include <vector>
+#include <fstream>
+#include <iostream>
 
-class Symbol {
-protected:
-   std::string mName;
-   std::vector<double> mParams;
-public:
-   Symbol (std::string const &name, std::vector<double> const &params) : 
-      mName {name},
-      mParams {params}
-   {}
-
-   std::vector<double> getParams () const {return mParams};
-   std::string getName () const {return mName};
-   void setParams (std::vector<double> const &newParams);
-
-};
+//class Symbol {
+//private:
+//   std::string mName;
+//   std::vector<double> mParams;
+//
+//public:
+//   Symbol (std::string const &name, std::vector<double> const &params) : 
+//      mName {name},
+//      mParams {params}
+//   {}
+//
+//   std::vector<double> getParams () const { return mParams; };
+//   std::string getName () const { return mName; };
+//   void setParams (std::vector<double> const &newParams);
+//
+//};
 
 class Token {
 protected:
+   union typeStorage {
+      typeStorage () {}
+      void setVal (int * const &intPtr) {mInt = intPtr; }
+      void setVal (double * const &floatPtr) {mFloat = floatPtr; }
+      void setVal (std::string * const &stringPtr) {mString = stringPtr; }
+      int * mInt;
+      double * mFloat;
+      std::string * mString;
+   } mTypeStorage;
+   //ADD DESTRUCTOR
+   
    enum token_type_t {
-      CHARACTER,
-      NUMBER,
-      SYMBOL
-   }
+      INT,
+      FLOAT,
+      IDENTIFIER,
+      OPERATOR,
+      CONDITION
+   };
 
    token_type_t mType;
 
-
-
+public:
+   Token (token_type_t const &type, int * const &intPtr) : mType { type } { mTypeStorage.setVal (intPtr); }
+   Token (token_type_t const &type, double * const &floatPtr) : mType { type } { mTypeStorage.setVal (floatPtr); }
+   Token (token_type_t const &type, std::string * const &stringPtr) : mType { type } { mTypeStorage.setVal (stringPtr); }
 };
+
+class IntToken : public Token {
+public:
+   IntToken (int * const &intPtr) : Token (INT, intPtr) {}
+   int * getData () const { return mTypeStorage.mInt; }
+};
+
+class FloatToken : public Token {
+public:
+   FloatToken (double * const &floatPtr) : Token (FLOAT, floatPtr) {}
+   double * getData () const { return mTypeStorage.mFloat; }
+};
+
+class IdentifierToken : public Token {
+public:
+   IdentifierToken (std::string * const &stringPtr) : Token (IDENTIFIER, stringPtr) {}
+   std::string * getData () const { return mTypeStorage.mString; }
+   ~IdentifierToken () { std::cout<<"deleting more pointers :)"<<std::endl; delete mTypeStorage.mString; }
+};
+
+class OperatorToken : public Token {
+public:
+   OperatorToken (std::string * const &stringPtr) : Token (OPERATOR, stringPtr) {}
+   std::string * getData () const { return mTypeStorage.mString; }
+};
+
+class ConditionToken : public Token {
+public:
+   ConditionToken (std::string * const &stringPtr) : Token (CONDITION, stringPtr) {}
+   std::string * getData () const { return mTypeStorage.mString; }
+};
+
+typedef std::vector<Token*> GLSstring;
 
 class Tokenizer {
 private:
-   std::vector<Symbol> symbolTable,
-                       axiom;
-   std::vector<Token> 
+   GLSstring mAxiom; 
+   std::vector<GLSstring> mProductions;
 
+   GLSstring readAxiom ( std::ifstream &configFile );
+   std::vector<GLSstring> readProductions ( std::ifstream &configFile );
+
+public:
+   Tokenizer ( std::ifstream &configFile ) : mAxiom {readAxiom (configFile)}, mProductions {readProductions (configFile)} {}
+   std::vector<GLSstring> getProductions () const { return mProductions; }
+   GLSstring getAxiom () const { return mAxiom; }
+   ~Tokenizer ();
 };
